a595bb87c06c9f089c1d7b23c1512783f2c30f6d
MirSiam10 <mir10siam@gmail.com>
2026-05-21 15:08:47 +0600

    stringfy Object

--- data.js
+++ data.js
@@ -13,9 +13,23 @@ const jsonData = `{
 
 console.log("JSON data as string:", jsonData);
 
+//Parse the JSON string into a JavaScript object
 const Parse = JSON.parse(jsonData);
 console.log(Parse);
 console.log("Name:", Parse.Name);
 console.log(typeof Parse);
 console.log(typeof jsonData);
 
+//Stringify the JavaScript object back into a JSON string
+
+const JSdata = {
+    Name: "Mir Siam",
+    Address: {
+        Street: "123 Main St",
+        City: "Anytown",
+        Country: "USA"
+    }
+};
+
+const JSONString = JSON.stringify(JSdata);
+console.log("JSONString:", JSONString);
